43460061d93bb23b31714c5041f30290223c91bc
carlopacioni <carlo.pacioni@gmail.com>
2025-05-09 14:08:04 +1000

    include exp term

--- Scripts/NeoModelProgAge.R
+++ Scripts/NeoModelProgAge.R
@@ -42,11 +42,11 @@ fitDetNeospora <- function(dir.in,
       dIc <- Ic
 
       if(theta<0) {
-        theta_neg <- theta * gr #* exp(abs(N/K))
+        theta_neg <- theta * gr * exp(abs(N/K))
         theta_pos <- 0
       } else {
         theta_neg <- 0
-        theta_pos <- theta * gr #* exp(theta/K)
+        theta_pos <- theta * gr * exp(theta/K)
       }
 
       rhoh <- alpha*zeta*(tIc/(tSc + tIc))
