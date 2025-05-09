532d0e80ae3fe6e1260c8ce04a98450b1cac0bc3
carlopacioni <carlo.pacioni@gmail.com>
2025-05-09 15:27:15 +1000

    include eps

--- Scripts/NeoModelProgAge.R
+++ Scripts/NeoModelProgAge.R
@@ -82,7 +82,7 @@ fitDetNeospora <- function(dir.in,
   # Ro <- (maxAge-3)*(1-delta)*alpha + alpha*(1-delta-eps)
   # G <- sum(3:maxAge)/length(3:maxAge)  # mean age of reproduction (generation time)
   # gr <- exp(log(Ro)/G)
-  gr <- 1 + abs(alpha - delta)
+  gr <- 1 + abs(alpha - delta - eps)
   
   params <- c(
     maxAge=maxAge,
