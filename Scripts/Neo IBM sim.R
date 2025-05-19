933b6f38bb874c55e0532c43ce84d5dd1d84aef6
carlopacioni <carlo.pacioni@gmail.com>
2025-05-19 16:48:40 +1000

    update testing

--- Scripts/Neo IBM sim.R
+++ Scripts/Neo IBM sim.R
@@ -38,7 +38,7 @@ parms <- expand.grid(list(maxAge=12,
                           rhov=0.59,
                           delta=0.02,
                           eps=0.1,
-                          sigma=c(0, 0.005, 0.01, 0.02),
+                          sigma=c(0, 0.005, 0.07),
                           zeta_env=0.03/alpha,
                           zeta_col=0.03/alpha,
                           p=0.3,
@@ -62,7 +62,7 @@ parms <- expand.grid(list(maxAge=12,
                           rhov=0.59,
                           delta=0.02,
                           eps=0.1,
-                          sigma=c(0, 0.005, 0.02, 0.07),
+                          sigma=c(0, 0.005, 0.07),
                           zeta_env=0.03/alpha,
                           zeta_col=0.03/alpha,
                           p=c(0, 0.3),
@@ -106,7 +106,7 @@ system.time(
 
 #### No Neo with intro ####
 parms <- expand.grid(list(maxAge=12,
-                          alpha=0.9, #
+                          alpha=alpha,
                           betas=0.05,
                           betaI=0.16,
                           rhov=0.59,
@@ -128,7 +128,32 @@ system.time(
                     root_name="NoNeoIntro", ncore="auto")
 )
 
+#### French et al ####
+parms <- expand.grid(list(maxAge=12,
+                          alpha=0.2/(1-0.16),
+                          betas=1-(0.2157/(0.2/(1-0.16))),
+                          betaI=0.16,
+                          rhov=0.95,
+                          delta=0.3,
+                          eps=0., ####
+                          sigma=c(0, 0.025), 
+                          zeta_env=c(0, 0.025/0.2), # to match publish value beta=0.03=alpha*zeta_env      
+                          zeta_col=0.0/alpha,
+                          p=0,
+                          g=1,
+                          InitPrev=0.02,
+                          K=150, ###
+                          c=0,        
+                          Se=0.98,
+                          Sp=0.99))
 
+
+system.time(
+  res_French <- proc_IBM(dir.in, intro, nsim, tot.time, parms, ageI=2, 
+                         root_name="res_French", ncore="auto")
+)
+res_French
+parms
 
 
 
@@ -137,4 +162,5 @@ system.time(
 debug(proc_IBM)
 debug(Neo.ibm)
 debug(schedule)
-
+debug(age.animals)
+debug(keepNconstant)
